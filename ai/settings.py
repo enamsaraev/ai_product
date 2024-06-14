68c27adf0a36f172e2114f46fa0a0f40f324c682
Evgeny Namsaraev <enamsaraev13@icloud.com>
2024-06-14 10:24:58 +0300

    debug

--- ai/settings.py
+++ ai/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [config('PROD_ALLOWED_HOSTS'), 'localhost', '127.0.0.1']
 
